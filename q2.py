b201ca480ffdbae628389aa48331b38a1d49961e
Gudipaty Aniket <annibhai000@gmail.com>
2022-03-21 17:30:37 +0530

    hope hai

--- q2.py
+++ q2.py
@@ -4,15 +4,16 @@ import re
 import pandas as pd
 from pyspark.sql import SparkSession
 import matplotlib.pyplot as plt
+import sys
 
 def f(s):
     data_arr = []
-    match = re.match(r'(.+) - - \[(\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})\] \"(\w+) .+\" (\d{3}) (\d+) .+', s)
-    match2 = re.match(r'(.+) - - \[(\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})\] \"(\w+) .+\" (\d{3}) (-) .+', s)
+    match = re.match(r'(.+) - (-|\w+) \[(\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})\] \"([A-Z]+) .+\" (\d{3}) (\d+|-) .+', s)
     if match:
-        data_arr = [match.group(1), match.group(2), match.group(3), int(match.group(4)), int(match.group(5))]
-    if match2:
-        data_arr = [match2.group(1), match2.group(2), match2.group(3), int(match2.group(4)), 0]
+        if match.group(6) == "-":
+            data_arr = [match.group(1), match.group(3), match.group(4), int(match.group(5)), 0]
+        else:
+            data_arr = [match.group(1), match.group(3), match.group(4), int(match.group(5)), int(match.group(6))]
         
     return data_arr
 
@@ -28,18 +29,19 @@ orig_count = data.count()
 
 # part C
 data = data.filter(lambda row: row != [])
-print("Number of bad rows: ", orig_count - data.count())
+print("Number of bad Rows: ", orig_count - data.count())
 
 # part D a
 d_1 = data.map(lambda x: (x[3], 1))
 d_1 = d_1.groupByKey().mapValues(len).sortByKey()
 
+sys.stdout = open("Da.txt", "w")
 print("HTTP status analysis:\nstatus    count")
 for x in d_1.collect():
     print(x[0], "     ", x[1])
+sys.stdout.close()
 
 # part D b
-
 tots = d_1.values().reduce(lambda x,y: x+y)
 d2 = d_1.map(lambda x : (x[0], x[1] / tots))
 
@@ -47,27 +49,33 @@ keys = d2.keys().collect()
 values = d2.values().collect()
 plt.figure()
 plt.pie(values, labels=keys, autopct='%1.1f%%', startangle=90)
-plt.savefig('http_status_analysis.png')
+plt.savefig('Db.png')
 
 # part D c
 d_3 = data.map(lambda x: (x[0], 1))
 d_3 = d_3.groupByKey().mapValues(len)
 
-print("Frequent hosts:\nhost        count")
+sys.stdout = open("Dc.txt", "w")
+print("Frequent hosts:\nhost                count")
 for x in d_3.collect():
-    print(x[0], "     ", x[1])
+    print(x[0],"               ", x[1])
+sys.stdout.close()
 
 # part D d
+sys.stdout = open("Dd.txt", "w")
 print("Unique hosts:\n", d_3.count())
+sys.stdout.close()
 
 # part D e
 date_dataset = data.map(lambda x: (x[1][0:11], x[0]))
 date_dataset = date_dataset.groupByKey().map(lambda x: (x[0], len(set(x[1])))).sortByKey()
 
+sys.stdout = open("De.txt", "w")
 print("Unique hosts per day:\nday               hosts")
 
 for x in date_dataset.collect():
     print(x[0], "     ", x[1])
+sys.stdout.close()
 
 # part D f
 
@@ -79,9 +87,7 @@ plt.plot(x, y)
 plt.xlabel('Day')
 plt.ylabel('Hosts Count')
 plt.title('No of unique hosts daily')
-plt.savefig('hosts_daily.png')
-# plt.show()
-
+plt.savefig('Df.png')
 
 # part D g
 
@@ -89,10 +95,11 @@ failure_data = data.filter(lambda x : 400 <= x[3] and x[3] < 600)
 failure_data = failure_data.map(lambda x: (x[0], 1))
 failure_data = failure_data.reduceByKey(lambda x,y: x+y).sortBy(lambda x: x[1], ascending=False)
 
+sys.stdout = open("Dg.txt", "w")
 print("Failed HTTP Clients:")
 for x in failure_data.take(5):
     print(x[0])
-
+sys.stdout.close()
 
 # part D h
 
@@ -108,7 +115,7 @@ plt.plot(total_data.keys().collect(), total_data.values().collect(), label='Tota
 plt.plot(failure_data.keys().collect(), failure_data.values().collect(), label='Failed Requests')
 plt.legend()
 plt.xticks(np.arange(total_data.keys().min(), total_data.keys().max() + 1, 1))
-plt.savefig('total_failure.png')
+plt.savefig('Dh.png')
 
 # plt.show()
 
@@ -118,19 +125,22 @@ my_data = data.map(lambda x: ((x[1][0:11], x[1][12:14]), 1))
 my_data = my_data.reduceByKey(lambda x,y: x+y).map(lambda x: (x[0][0], (x[0][1], x[1])))
 my_data = my_data.reduceByKey(lambda x,y: x if x[1] > y[1] else y).sortByKey()
 
+sys.stdout = open("Di.txt", "w")
 print("Active Hours:")
 
 for x in my_data.collect():
     print(x[0], "     ", x[1][0] + ":00")
-
+sys.stdout.close()
 
 # part D j
 
 col = data.map(lambda x : x[4])
 
+sys.stdout = open("Dj.txt", "w")
 print("Response Length Statistics:")
 print("Minimum length", col.min())
 print("Maximum length", col.max())
 print("Average length", col.mean())
+sys.stdout.close()
 
 spark.stop()
